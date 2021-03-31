4e60b8b32fd8a6ec0b3d4e302c55b4c423da771d
unknown <ianc99988@gmail.com>
2021-03-31 00:01:02 -0700

    Added a loading page that gets rendered right after capture button clicked, added top 2 predictions and prob

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 import torch 
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 app = Flask(__name__) # instantiates a Flask application
@@ -32,18 +33,17 @@ def generate_frames():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             yield (img)
 
-# get image for model prediction, ret_type = 'bytes' or 'array'
-def get_image(ret_type): 
+# get image for model prediction
+# returns img_bytes(in bytes), img_arr(in array)
+def get_image(): 
     success, frame = camera.read()
+    img_arr = frame
     if success:
-        if (ret_type == 'bytes'):
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes() # converting to bytes otherwise Flask will complain 
-            img = (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            return img
-        elif (ret_type == 'array'):
-            return frame
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes() # converting to bytes otherwise Flask will complain 
+        img_bytes = (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        return img_bytes, img_arr 
 
 # gets the prediction of the mood from the image 
 def prediction(img):
@@ -68,19 +68,23 @@ def prediction(img):
 
     # put image through model and obtain prediction 
     pred = vgg_model(img)
-    print(pred)
+    prob = F.softmax(pred, dim=1)[0]
+    # gets the emotion with the top probability
+    first_value, first_index = prob.max(0)
+    prob[first_index] = 0
+    # gets the emotion with the second best probability
+    second_value, second_index = prob.max(0)
 
     max = 0
     label_test = ["angry", "fatigue", "fear", "happy", "neutral", "sad", "surprise"]
+
     for i, x in enumerate(pred):
         for j, y in enumerate(pred[i]):
             if(y > max):
                 max = y
                 label = label_test[j]
-    print(label)
-    print(pred)
 
-    return label
+    return label, first_value.item() * 100, second_value.item() * 100, label_test[first_index.item()], label_test[second_index.item()]
 
 @app.route('/image')
 def image():
@@ -90,17 +94,21 @@ def image():
 @app.route('/result')
 def result():
     global display_label
-    display_label = prediction(predict_img)
-    return render_template('result.html', label=display_label)
+    display_label, first_perc, second_perc, first_value, second_value = prediction(predict_img)
+    return render_template('result.html', label=display_label, first_perc=first_perc, first_value=first_value, second_perc=second_perc, second_value=second_value)
+
+# the temporary template that will be loaded when the user presses the capture button, will then render the results route 
+@app.route('/loading')
+def loading():
+    return render_template('loading.html', image=display_img)
     
 @app.route('/button_capture', methods=["GET", "POST"])
 def button_capture():
     # save the image captured in display_img and the prediction image in predict_img
     global display_img
     global predict_img
-    display_img = get_image('bytes')
-    predict_img = get_image('array')
-    return redirect(url_for('result'))
+    display_img, predict_img = get_image()
+    return redirect(url_for('loading'))
 
 @app.route('/button_again', methods=["GET", "POST"])
 def button_again():   
